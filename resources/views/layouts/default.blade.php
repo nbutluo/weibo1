9bb257e5b46c7320594a5a3fd8b96edd40c65ee4
nbutluo <nbutluo@163.com>
2020-08-27 03:34:40 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,21 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title','Weibo App')</title>
+    <title>@yield('title','Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-<h1>@yield('content')</h1>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.default')
 @section('content')
-    主页
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, Laravel!</h1>
+        <p class="lead">你现在所看到的是 <a href="https://learnku.com/docs/laravel/5.7" target="_blank">Laravel 入门教程</a></p>
+        <hr class="my-4">
+        <p>一切从这里开始</p>
+        <a class="btn btn-success btn-lg" href="#" role="button">点击去注册</a>
+    </div>
 @endsection
