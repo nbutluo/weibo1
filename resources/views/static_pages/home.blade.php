24145d489a9193b0d6ec7bbbb1fdf92af15b253e
nbutluo <nbutluo@163.com>
2020-08-27 12:51:18 +0000

    用户注册

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,6 +5,6 @@
         <p class="lead">你现在所看到的是 <a href="https://learnku.com/docs/laravel/5.7" target="_blank">Laravel 入门教程</a></p>
         <hr class="my-4">
         <p>一切从这里开始</p>
-        <a class="btn btn-success btn-lg" href="#" role="button">点击去注册</a>
+        <a class="btn btn-success btn-lg" href="{{ route('signup') }}" target="_blank" role="button">点击去注册</a>
     </div>
 @endsection
