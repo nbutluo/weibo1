7a2451705b8c110498c2520d3791783ff18a5ffd
nbutluo <nbutluo@163.com>
2020-09-05 10:57:40 +0800

    补充 用户账户激活

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(User::class)->times(500)->make();
+        $users = factory(User::class)->times(50)->make();
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
